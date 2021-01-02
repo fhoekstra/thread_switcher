d68a2ccc62d5325de6092d8fdb5d423dbd88ec8b
Freek Hoekstra <freekgeorgehoekstra@gmail.com>
2021-01-02 15:07:09 +0100

    Moved threadswitcher to Python

--- CPULoadThreadSweepCinebench.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:LOOP
-for /f %%i in (.\threadlist.txt) do Powershell "ForEach($PROCESS in GET-PROCESS prime95) { $PROCESS.ProcessorAffinity=%%i}" | ping -n 10 127.0.0.1
-GOTO LOOP
--- main.py
+++ main.py
@@ -1,21 +1,87 @@
 from subprocess import call
 import time
 
-#### Configuration here ####
-process_to_switch = "prime95"
-thread_num = 12  # number of threads
-sec_between_switch = 10  # number of seconds between switching threads
-############################
 
-thread_list = [2**n for n in range(thread_num)]
+cfg = {
+    ##########################################################################
+    "process_to_switch": "prime95",  # name of process                       #
+    "thread_num": 12,  # number of threads                                   #
+    "sec_between_switch": 10,  # number of seconds between switching threads #
+    "hyper_threading": True,                                                 #
+    ##########################################################################
+}
 
-running = True
-while running:
+
+class Thread:
+    def __init__(self, total_num: int,
+                 index: int = None, friendly_number: int = None,
+                 hyper_threading: bool = True):
+        """
+        total_num is total number of threads.
+        index is zero-indexed thread index
+        friendly_number is 1-indexed thread index
+        """
+        if index is None and friendly_number is None:
+            raise ValueError("One of index, friendly_number must be"
+                             " provided as an int")
+        if ((index is not None) and (friendly_number is not None)
+                and (friendly_number != index + 1)):
+            raise ValueError("index must be 1 lower than friendly_number")
+
+        self.total_num = total_num
+
+        if index is not None:
+            self.index = index
+            self.friendly_number = index + 1
+        else:
+            self.friendly_number = friendly_number
+            self.index = friendly_number - 1
+
+        if hyper_threading:
+            assert self.total_num % 2 == 0
+            self.core_index = int(self.index / 2)
+            self.core_friendly_number = self.core_index + 1
+            self.core_total = int(self.total_num / 2)
+        else:
+            self.core_index, self.core_friendly_number, self.core_total = \
+                self.index, self.friendly_number, self.total_num
+
+    def __repr__(self):
+        return (f"Thread {self.friendly_number}/{self.total_num} ;"
+                f"Core {self.core_friendly_number}/{self.core_total}")
+
+    @property
+    def affinity_mask(self):
+        return str(2**self.index)
+
+
+def get_infinite_iterator(collection: list):
+    length = len(collection)
+    i = 0
+    while True:
+        if i >= length:
+            i = 0
+        yield collection[i]
+        i += 1
+
+
+def main(cfg: dict):
+    thread_list = [Thread(cfg["thread_num"], index=n)
+                   for n in range(cfg["thread_num"])]
+
     try:
-        # do switch
-        time.sleep(sec_between_switch)
-        print(f"{sec_between_switch} s passed")
-    except KeyboardInterrupt:
-        running = False
+        for thread in get_infinite_iterator(thread_list):
+            print(f"switching to {thread}")
+            call('Powershell "ForEach($PROCESS in'
+                 + f' GET-PROCESS {cfg["process_to_switch"]})'
+                 + ' { $PROCESS.ProcessorAffinity=' + thread.affinity_mask
+                 + '}"')
+            time.sleep(cfg["sec_between_switch"])
+    except KeyboardInterrupt as e:
+        print("Stopped")
+        raise(e)
+        quit()
 
-print("Stopped")
+
+if __name__ == '__main__':
+    main(cfg)
