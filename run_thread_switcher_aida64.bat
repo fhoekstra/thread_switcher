4eafd52fc8eaa484b9453d973fbe9a12a85980ab
coffeepenbit <coffeepenbit@gmail.com>
2022-02-22 08:09:11 -0500

    Comment on python invocation

--- run_thread_switcher_aida64.bat
+++ run_thread_switcher_aida64.bat
@@ -2,6 +2,7 @@ pushd %~dp0
 
 copy /Y settings_aida64.py settings.py
 
+:: You may also replace this with `python` or `python3X` if you are having issues
 py main.py
 
 popd
--- run_thread_switcher_prime95.bat
+++ run_thread_switcher_prime95.bat
@@ -1,3 +1,4 @@
 copy /Y settings_prime95.py settings.py
 
+:: You may also replace this with `python` or `python3X` if you are having issues
 py main.py
